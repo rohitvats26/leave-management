1088c1c8d0ab70cb4e2dd94ba18576b92188ef79
Nitin <rohitvats26@gmail.com>
2026-06-04 01:27:51 +0530

    fix tracing issue

---
+++ leave-service/src/main/java/com/lms/leave/config/TracingConfig.java
@@ -0,0 +1,29 @@
+package com.lms.leave.config;
+
+import io.micrometer.observation.ObservationPredicate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.observation.ServerRequestObservationContext;
+
+import java.util.List;
+
+@Configuration
+public class TracingConfig {
+
+    private static final List<String> IGNORED_PATHS = List.of(
+            "/actuator",
+            "/h2-console",
+            "/eureka"
+    );
+
+    @Bean
+    public ObservationPredicate skipNoisyPaths() {
+        return (name, context) -> {
+            if (context instanceof ServerRequestObservationContext ctx) {
+                String uri = ctx.getCarrier().getRequestURI();
+                return IGNORED_PATHS.stream().noneMatch(uri::startsWith);
+            }
+            return true;
+        };
+    }
+}
